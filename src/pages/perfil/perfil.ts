2591fc47601a5a0dbf07d94c3045912ac216324a
waltenes-ps <waltenesjr@live.com>
2018-06-06 16:02:38 -0300

    ajuste de icones

--- src/pages/perfil/perfil.ts
+++ src/pages/perfil/perfil.ts
@@ -15,13 +15,47 @@ import {NavController, NavParams} from 'ionic-angular';
 export class PerfilPage {
 
   etapa: number;
+  quantPergunA: number;
+  quantPergunB: number;
+  quantPergunC: number;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.etapa = 1;
+    this.quantPergunA = 0;
+    this.quantPergunB = 0;
+    this.quantPergunC = 0;
   }
 
-  changeList(evento) {
-    alert(evento);
+  changeList(pergunta: string) {
+    this.etapa++;
+    this.contarPergunta(pergunta);
+  }
+
+  getResult(pergunta: string) {
+    this.contarPergunta(pergunta);
+    if (this.quantPergunA > this.quantPergunB) {
+      if (this.quantPergunA > this.quantPergunC) {
+        alert('Pergunta A')
+      } else {
+        alert('pergunta C')
+      }
+    } else {
+      if (this.quantPergunB > this.quantPergunC) {
+        alert('Pergunta B')
+      } else {
+        alert('pergunta C')
+      }
+    }
+  }
+
+  contarPergunta(pergunta: string) {
+    if (pergunta === 'a') {
+      this.quantPergunA++;
+    } else if (pergunta === 'b') {
+      this.quantPergunB++;
+    } else {
+      this.quantPergunC++;
+    }
   }
 
   ionViewDidLoad() {
