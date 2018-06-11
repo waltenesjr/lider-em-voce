92c4af35a2d24686abdf47337a65a4f4f4c1c377
waltenes-ps <waltenesjr@live.com>
2018-06-11 13:10:26 -0300

    background toobar utilizando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,13 +24,13 @@ export class MyApp {
   }
 
   initializeApp() {
-    this.redirecionar();
-    this.configPush();
     this.platform.ready().then(() => {
+      this.redirecionar();
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.configPush();
     });
   }
 
